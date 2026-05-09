0eed949d5f52a8095ee421f748f0a14e9a97a9f2
nphatcaythue11-ai <nphatcaythue11@gmail.com>
2026-05-09 18:29:16 +0700

    Update menu.js

--- src/commands/menu.js
+++ src/commands/menu.js
@@ -2,11 +2,13 @@ function sendMainMenu(bot, chatId) {
     const options = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
-                [{ text: '💸 Bảng Giá', callback_data: 'menu_price' }, { text: '🎮 Mua Acc', callback_data: 'menu_acc' }],
-                [{ text: '🚀 Cày Thuê', callback_data: 'menu_boost' }]
+                [{ text: '🎁 Bảng Giá Gamepass / Perm', callback_data: 'menu_gp' }],
+                [{ text: '⏳ Robux 120H', callback_data: 'menu_rb120' }, { text: '🎮 Mua Acc', callback_data: 'menu_acc' }],
+                [{ text: '🚀 Cày Thuê', callback_data: 'menu_boost' }, { text: '🎧 Gọi Sếp Phát', callback_data: 'menu_admin' }]
             ]
         })
     };
     bot.sendMessage(chatId, "Chào đằng ấy! Chọn dịch vụ bên dưới để tớ tư vấn lẹ cho nha 👇", options);
 }
+
 module.exports = { sendMainMenu };
